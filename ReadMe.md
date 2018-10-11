4d5b6604840b15bf9a3ebe0fadba3226e43a4f16
anjan <anjan@w3engineers.com>
2018-10-11 12:25:02 +0600

    data binding

--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,52 @@
-### important Links
+### Include Layout
+
+Databinding allows to include any layout
+
+    <include
+            layout="@layout/content_recycler"
+            android:id="@+id/content"
+            bind:user="@{user}"
+            />
+
+### Import
+
+Databinding Allows to import any class
+
+    <import type="com.example.user06.databinding.recycler.utils.BindingUtils"/>
+    <TextView
+                android:id="@+id/textView"
+                ...
+                android:text="@{BindingUtils.convertToSuffix(user.numberOfFollowers)}"
+                ...
+                tools:textColor="@android:color/white" />
+
+### Binding Adapters
+
+BindingAdapter annotation allows to customize how a setter for an attribute is called.
+
+    @BindingAdapter({"profileImage"})
+        public static void loadImage(ImageView view, String imageUrl){
+
+            Glide.with(view.getContext())
+                    .load(imageUrl)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(view);
+        }
+Above example will load image url by using the attribute **profileImage** used anywhere in layout! (in whole app of course).
+
+        <ImageView
+            android:id="@+id/imageView3"
+            ....
+            bind:profileImage="@{user.profileImage}"
+            ....
+            app:srcCompat="@drawable/profile_img" />
+
+
+### Click Handlers
+
+
+
+### Reference Links
 
 - https://inducesmile.com/android/android-databinding-example-tutorial/
 
@@ -8,4 +56,6 @@
 
 
 
-### Binding Adapters
+Thanks
+
+Anjan Debnath
--- app/src/main/java/com/example/user06/databinding/recycler/activity/RecyclerActivity.java
+++ app/src/main/java/com/example/user06/databinding/recycler/activity/RecyclerActivity.java
@@ -1,9 +1,8 @@
 package com.example.user06.databinding.recycler.activity;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,10 +11,13 @@ import com.example.user06.databinding.R;
 import com.example.user06.databinding.databinding.ActivityRecyclerBinding;
 import com.example.user06.databinding.recycler.adapter.PostsAdapter;
 import com.example.user06.databinding.recycler.model.Post;
+import com.example.user06.databinding.recycler.model.User;
 
 public class RecyclerActivity extends AppCompatActivity implements PostsAdapter.PostsAdapterListener {
 
     ActivityRecyclerBinding activityRecyclerBinding;
+    private User user;
+    CustomClickHandlers clickHandlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +31,54 @@ public class RecyclerActivity extends AppCompatActivity implements PostsAdapter.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        renderProfile();
+    }
+
+
+    private void renderProfile(){
+
+        user = new User();
+        user.setName("David Attenborough");
+        user.setEmail("david@natgeo.com");
+        user.setProfileImage("https://api.androidhive.info/images/nature/david.jpg");
+        user.setAbout("Naturalist");
+
+        // ObservableField doesn't have setter method, instead will
+        // be called using set() method
+        user.numberOfPosts.set(3400L);
+        user.numberOfFollowers.set(3050890L);
+        user.numberOfFollowing.set(150L);
+
+
+        // display user
+        activityRecyclerBinding.setUser(user);
+
+
     }
 
     @Override
     public void onPostClicked(Post post) {
+
+    }
+
+    public class CustomClickHandlers{
+
+        Context mContext;
+        public CustomClickHandlers(Context context){
+            mContext = context;
+        }
+
+        public void onProfileInfoFabClicked(View view){
+
+            user.setName("Sir David Attenborough");
+            user.setProfileImage("https://api.androidhive.info/images/nature/david1.jpg");
+
+            // updating ObservableField
+            user.numberOfPosts.set(5500L);
+            user.numberOfFollowers.set(5050890L);
+            user.numberOfFollowing.set(180L);
+
+        }
 
     }
 }
